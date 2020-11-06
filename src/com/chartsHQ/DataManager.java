69d644b88529f15d7bda72944c92bed4be38faff
Surajit Kundu <surajit.113125@gmail.com>
2020-11-06 10:22:25 +0530

    Comments added

--- src/com/chartsHQ/DataManager.java
+++ src/com/chartsHQ/DataManager.java
@@ -23,37 +23,71 @@ import org.json.simple.parser.JSONParser;
 public class DataManager {
     JSONArray data = new JSONArray();
     JSONArray schema = new JSONArray();
-    public void loadData(String dataPath) throws Exception{
-        //Check correct JSON file validation
-        JSONParser parser = new JSONParser();
-        JSONArray jsonData = (JSONArray) parser.parse(new FileReader(dataPath));
-        this.data = jsonData;
+    
+    /**
+     * Load the data object of the given JSON path.
+     *
+     * @param dataPath
+     *              The dataPath must be a valid JSON data source path.
+     * @throws Exception
+     *             If there is an invalid JSON or unable to parse the JSON.
+     */ 
+    public void loadData(String dataPath) throws Exception{ 
+        JSONParser parser = new JSONParser(); 
+        JSONArray jsonData = (JSONArray) parser.parse(new FileReader(dataPath));// Parses the json data
+        this.data = jsonData;   //Set the JSON data in global variable
     }
+    
+    /**
+     * Load the schema object of the given JSON path.
+     *
+     * @param schemaPath
+     *              The schemaPath must be a valid JSON schema file.
+     * @throws Exception
+     *             If there is an invalid JSON or unable to parse the JSON.
+     */     
     public void loadSchema(String schemaPath) throws Exception{
         JSONParser parser = new JSONParser();
-        JSONObject jsObj = (JSONObject) parser.parse(new FileReader(schemaPath));
+        JSONObject jsObj = (JSONObject) parser.parse(new FileReader(schemaPath));// Parses the json data
         JSONArray jsonSchema = (JSONArray) jsObj.get("schema");
-        this.schema = jsonSchema;
+        this.schema = jsonSchema; //Set the schema information in global variable
     }
-    public String show() throws Exception{
+    
+    /**
+     * View the dataset .
+     *
+     * 
+     * @return The entire dataset.
+     *
+     */
+    public String show(){
         String dataSet="";
         int dataSetLength = data.size();
         String keySet="", values="";
-        boolean isHeaderSet = false;
-        for(int i=0;i<dataSetLength;i++){
-            JSONObject jsObj = (JSONObject) data.get(i);
-            for(Object key:jsObj.keySet()){
+        boolean isHeaderSet = false;    //Weather column namnes are set on the top of the data
+        for(int i=0;i<dataSetLength;i++){  //Loop for reading each JSON Object  
+            JSONObject jsObj = (JSONObject) data.get(i); //Parsing each data object from JSONArray
+            for(Object key:jsObj.keySet()){    //Loop for reading value of the corresponding key
                 if(!isHeaderSet)
-                   keySet += key+" , "; 
-                values += jsObj.get(key)+" , ";      
+                   keySet += key+" , ";     //Get the Column names
+                values += jsObj.get(key)+" , ";    //Get the value of all dimensions and measures  
             }
             isHeaderSet = true;
-            values=values.substring(0, values.lastIndexOf(","))+"\n";
+            values=values.substring(0, values.lastIndexOf(","))+"\n";   //Remove the last "," and add line break;
         }
-        keySet=keySet.substring(0, keySet.lastIndexOf(","))+"\n";
+        keySet=keySet.substring(0, keySet.lastIndexOf(","))+"\n";   //Remove the last "," and add line break;
         dataSet=keySet+values;
         return dataSet;
     }
+    /**
+     * Project the dataset on given column name.
+     *
+     * @param col[]
+     *             col[] is the list of columns, which want to project.
+     * 
+     * @return The corresponding the of the column.
+     *
+     */    
     public String project(String col[]) throws Exception{
         String dataSet="";
         int dataSetLength = data.size();
@@ -71,6 +105,15 @@ public class DataManager {
         dataSet=keySet+values;
         return dataSet;
     }
+    /**
+     * Group the dataset on given dimension.
+     *
+     * @param col
+     *             col is the dimension name.
+     * 
+     * @return The aggregated ( mean ) according the dimension.
+     *
+     */     
     public List GroupBy(String col){
         List<String> measures = new ArrayList();
         HashMap<String, Vector<Vector>> measureDataSet = new HashMap();
@@ -79,7 +122,7 @@ public class DataManager {
         columnNames.add(col);
         try{
             JSONArray jsArrSchema = schema;
-            for(int i=0;i<jsArrSchema.size();i++){
+            for(int i=0;i<jsArrSchema.size();i++){  //Create a list of measure columns
                 JSONObject element = (JSONObject) jsArrSchema.get(i);
                 if(element.get("type").equals("dimension") && element.get("name").equals(col)){
                     isValidDimensionColumn = true;
@@ -88,7 +131,7 @@ public class DataManager {
                     measures.add(String.valueOf(element.get("name")));
                     columnNames.add(String.valueOf(element.get("name")));
                 }
-            } //Create a list of measure columns 
+            }  
             
             JSONArray jsDataSet = data;
             int jsDataSetLength = jsDataSet.size();           
@@ -97,47 +140,67 @@ public class DataManager {
                 Vector measureDataList = new Vector();
                 Vector measuresData = new Vector();
                 for(Object key:measures){
-                    measuresData.add(groupListObj.get(key));    
+                    measuresData.add(groupListObj.get(key));  //add the value of measures in vector  
                 }
-                if(measureDataSet.get(String.valueOf(groupListObj.get(col))) == null){
+                if(measureDataSet.get(String.valueOf(groupListObj.get(col))) == null){ //If the hasmap values is empty of the corresponding key
                     measureDataList.add(measuresData);
                     measureDataSet.put(String.valueOf(groupListObj.get(col)), measureDataList);
                 }
-                else{
+                else{   //If there is already some value in has table, get these and add the new.
                     measureDataList = measureDataSet.get(String.valueOf(groupListObj.get(col)));
                     measureDataList.add(measuresData);
                     measureDataSet.put(String.valueOf(groupListObj.get(col)), measureDataList);
                 }
             }
-            return avg(columnNames, measureDataSet, measures.size());            
+            return avg(columnNames, measureDataSet, measures.size());   //return the agregated mean         
         }catch(Exception ex){System.err.print(ex); return null;} 
     }
 
-    public static List avg(Vector columnNames, HashMap<String, Vector<Vector>> data, int measureSize){
+    
+    /**
+     * Get the mean of the group of measure
+     *
+     * @param columnNames, data, easureSize
+     *             columnNames is the list of column needs to be projected.
+     *             data is the Hash map of the group of object.
+     *             measureSize is the number of measure column.
+     * 
+     * @return The aggregated ( mean ) according the dimension.
+     *
+     */     
+    public List avg(Vector columnNames, HashMap<String, Vector<Vector>> data, int measureSize){
         List<String> result = new ArrayList();
-        Pattern patternInt = Pattern.compile("[+-]?[0-9]+");
-        //Pattern patternDouble = Pattern.compile("[+-]?[0-9]*\\.?[0-9]*");
         result.add(columnNames+"\n");
         for(String key:data.keySet()){
             Vector v = data.get(key);
             Vector rows = new Vector();
             rows.add(key);
-            for(int j=0;j<measureSize;j++){
-                double sum=0;
-                 double valueDouble = 0.0;  
+            for(int j=0;j<measureSize;j++){ //Sum elements at same index of the vectors into a single vector
+                double sum=0; 
                  for(int i=0;i<v.size();i++){
+                     double valueDouble = 0.0;
                      Vector v1 = (Vector) v.get(i);
-                     if(!String.valueOf(v1.get(j)).equals("null") || v1.get(j)!=null)
+                     if(!String.valueOf(v1.get(j)).equals("null") || v1.get(j)!=null) //If null, it will be 0.0 (default)
                          valueDouble = Double.parseDouble(v1.get(j).toString());
                      sum += valueDouble;
                  }
-                 rows.add(sum);
+                 rows.add(sum/v.size()); //Calculating mean
             }
             result.add(rows+"\n");
         }
         return result;
     }
-
+    
+    /**
+     * Project the dataset based on following condition
+     *
+     * @param col, predicate
+     *             col is the name of the column on which we want to add filter.
+     *             predicate is the condition.
+     * 
+     * @return List of filtered dataset.
+     *
+     */ 
     public List select(String col, Predicate<Double> predicate){
         List<String> result = new ArrayList();
         try{
@@ -150,7 +213,7 @@ public class DataManager {
                 //String keySet="", values="";
                 Vector values = new Vector();
                 JSONObject jsObj = (JSONObject) jsDataSet.get(i);
-                if(predicate.test(Double.valueOf(jsObj.get(col).toString()))){
+                if(predicate.test(Double.valueOf(jsObj.get(col).toString()))){ //Evaluates the condition
                 for(Object key:jsObj.keySet()){
                         if(!isHeaderSet)
                            keySet.add(key); 
